f8328a0cb88915c34c0ce57dca855706f984c9fb
Abhi Agarwal <me@abhiagarwal.com>
2017-07-11 21:15:50 -0400

    hourly auth

--- index.js
+++ index.js
@@ -1,28 +1,71 @@
+'use strict';
+
+var elasticsearch = require('elasticsearch');
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
 var Q = require('q');
 
+// Instantiate a elasticsearch client
+var elasticSearchClient = new elasticsearch.Client({
+    host: 'https://newsai:XkJRNRx2EGCd6@search.newsai.org',
+    // log: 'trace',
+    rejectUnauthorized: false
+});
+
 app.get('/', function(req, res) {
     res.sendFile(__dirname + '/index.html');
 });
 
-function validateUser(userId, accessToken) {
+function validateUser(userId, authToken) {
     var deferred = Q.defer();
+
+    elasticSearchClient.get({
+        index: 'users',
+        type: 'user',
+        id: userId
+    }, function(error, response) {
+        if (error) {
+            console.error(error);
+            deferred.reject(error);
+        } else {
+            if (response._source && response._source.data && response._source.data.LiveAccessToken) {
+                if (authToken === response._source.data.LiveAccessToken) {
+                    deferred.resolve(true);
+                } else {
+                    var error = 'Live Access Token is invalid';
+                    console.error(error);
+                    deferred.reject(error);
+                }
+            } else {
+                var error = 'User does not have live access token';
+                console.error(error);
+                deferred.reject(error);
+            }
+        }
+    });
 
     return deferred.promise;
 }
 
+String.prototype.hashCode = function() {
+    var hash = 5381, i = this.length;
+    while (i)
+        hash = (hash * 33) ^ this.charCodeAt(--i)
+    return hash >>> 0;
+}
+
 function generatePageTeamIdHash(page, teamId) {
-    
+    var hash = page + teamId;
+    return hash.hashCode();
 }
 
 io.on('connection', function(socket) {
     // On authentication connection (Initial connection)
     socket.on('auth', function(authDetails) {
         // Validate if their authentication is valid
-        validateUser(authDetails.userId, authDetails.accessToken).then(function(status) {
+        validateUser(authDetails.userId, authDetails.authToken).then(function(status) {
             // Join the current page they are on (room)
             // Hash of their current page and team id
             var roomName = generatePageTeamIdHash(authDetails.page, authDetails.teamId);
@@ -30,11 +73,15 @@ io.on('connection', function(socket) {
 
             // Respond back to the client that their
             // Authentication is successful
-            socket.json.send({'Status': 'Success'});
+            socket.json.send({
+                'Status': 'Success'
+            });
         }, function(error) {
             // If it is not valid then disconnect their socket
             // And message them back
-            socket.json.send({'Status': 'Failure'});
+            socket.json.send({
+                'Status': 'Failure'
+            });
             socket.disconnect(true);
         });
     });
@@ -48,11 +95,11 @@ io.on('connection', function(socket) {
         if (changeDetails.resourceName === 'page') {
             // Remove from all other page rooms that this socket is currently in
             var rooms = io.sockets.adapter.sids[socket.id];
-            for(var room in rooms) {
+            for (var room in rooms) {
                 if (room !== socket.id) {
                     socket.leave(room);
                 }
-            } 
+            }
 
             // If there is a page they have changed to then
             // add them to that room
